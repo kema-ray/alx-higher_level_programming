e760189871efe77f0a14497d9123eb29232636b4
kema-ray <rachelkemuma99@gmail.com>
2023-01-24 13:22:48 +0300

    a class Square that defines a square of Private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module"""
+
+class Square:
+    """Declares a square class"""
+    def __init__(self, size):
+        """
+        Initializes class atttributes
+        Args:
+            size: size of square
+        """
+        self.__size = size
