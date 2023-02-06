3a232f53676ea59c219e48a026e225abcbb805b0
kema-ray <rachelkemuma99@gmail.com>
2023-02-06 16:17:26 +0300

    public instance method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for the base_geometry
+"""
+
+
+class BaseGeometry:
+    """Class with a public attribute area"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
