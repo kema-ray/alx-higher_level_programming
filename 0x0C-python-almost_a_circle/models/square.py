c4aee4d105f05bf9bb03821857465c457efbeb3a
kema-ray <rachelkemuma99@gmail.com>
2023-02-13 14:30:03 +0300

    method  that returns the dictionary representation of a square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -65,3 +65,10 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+    
+    def to_dictionary(self):
+        """
+        returns a dictionary representation of the rectangle
+        """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 
+                'y': self.y}
