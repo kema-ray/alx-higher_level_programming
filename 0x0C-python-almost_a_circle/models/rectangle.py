0a08eaee0601e8d897dc6fc666ec63405347ba6c
kema-ray <rachelkemuma99@gmail.com>
2023-02-13 14:25:54 +0300

    method  that returns the dictionary representation of a Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,10 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        returns a dictionary representation of the rectangle
+        """
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
