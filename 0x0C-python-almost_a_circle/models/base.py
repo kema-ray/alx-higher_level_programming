04c02c6bd6b0d18dd880bb4190c3243c471aeef7
kema-ray <rachelkemuma99@gmail.com>
2023-02-13 16:42:40 +0300

    static method that returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Class Module
 """
+import json
 
 
 class Base:
@@ -42,3 +43,10 @@ class Base:
             raise TypeError('{} must be an integer'.format(name))
         if value < 0:
             raise ValueError('{} must be >= 0'.format(name))
+
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        return json.dumps(list_dictionaries or [])
