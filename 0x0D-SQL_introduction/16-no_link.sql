21873b755c4058e6ad6ea2cbb8991a725021252d
kema-ray <rachelkemuma99@gmail.com>
2023-03-15 05:56:11 +0300

    lists all records of the table second_table

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table second_table
+-- Query to list all records of the table second_table
+
+SELECT score, name FROM second_table WHERE name IS NOT NULLORDER BY score DESC;
