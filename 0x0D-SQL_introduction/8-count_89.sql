c564c345f098c0b11b920d6936beba134ed4a1c7
kema-ray <rachelkemuma99@gmail.com>
2023-03-15 05:31:46 +0300

    displays the number of records with id = 89 in the table first_table

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the number of records with id = 89 in the table first_table
+-- Query to display the number of records with id = 89 in the table first_table
+
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
