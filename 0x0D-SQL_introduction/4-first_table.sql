e995cc52cfb80e3e9ae3e83c5834bc58a795cab8
kema-ray <rachelkemuma99@gmail.com>
2023-03-15 05:20:18 +0300

    creates a table called first_table in the current database in your MySQL server

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table
+-- Query to create a table called first_table in the current database in your MySQL server
+
+CREATE TABLE IF NOT EXISTS first_name (
+id INT,
+name VARCHAR(256));
