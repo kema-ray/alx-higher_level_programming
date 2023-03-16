82480280f8021056c94d00c319d5276905e6eee3
kema-ray <rachelkemuma99@gmail.com>
2023-03-16 05:38:49 +0300

    fix code

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,9 @@
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
 CREATE TABLE IF NOT ES=XISTS 'hbtn_0d_usa'.'cities' (
 		PRIMARY KEY('id'),
-		'id'	INT	NOT NULL AUTO_INCREMENT,
+		'id'INT	NOT NULL AUTO_INCREMENT,
 		'state_id'	INT	NOT NULL,
-		'name'	VARCHER(256)	NOT NULL,
+		'name'	VARCHAR(256)	NOT NULL,
 		FOREIGN KEY('state_id')
 		REFERENCE 'hbtn_0d_usa'.'states'('id')
 );
