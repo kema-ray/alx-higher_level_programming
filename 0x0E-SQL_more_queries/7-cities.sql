dbcc8fc1d98e4661648bee9471751add70df90ec
kema-ray <rachelkemuma99@gmail.com>
2023-03-16 05:40:23 +0300

    fix code

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,11 +1,8 @@
 --  script that creates the database hbtn_0d_usa and the table cities
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT ES=XISTS 'hbtn_0d_usa'.'cities' (
-		PRIMARY KEY('id'),
-		'id'INT	NOT NULL AUTO_INCREMENT,
-		'state_id'	INT	NOT NULL,
-		'name'	VARCHAR(256)	NOT NULL,
-		FOREIGN KEY('state_id')
-		REFERENCE 'hbtn_0d_usa'.'states'('id')
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+       	state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
 );
