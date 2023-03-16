af9de9c3ab9b3e1fa029bf81fd3f3d7db1aef7bf
kema-ray <rachelkemuma99@gmail.com>
2023-03-16 06:10:41 +0300

    script that lists all the tv shows that can be found in the database

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
---script that lists all shows contained in hbtn_0d_tvshows
+-- A script that lists all the tv shows that can be found in the database
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
