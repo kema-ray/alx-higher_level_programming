dcaf47dc4128f7d585ca9a32033c292ea137bd8d
kema-ray <rachelkemuma99@gmail.com>
2023-04-14 15:35:08 +0300

    fix semistandard validation

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-	const i = list.length;
-	const reverseList = new Array(i);
+  const i = list.length;
+  const reverseList = new Array(i);
 
-	list.forEach((item, n) => {
-			reverseList[i - n - 1] = item;
-			});
-	return reverseList;
+  list.forEach((item, n) => {
+    reverseList[i - n - 1] = item;
+  });
+  return reverseList;
 };
