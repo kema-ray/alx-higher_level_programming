e125e452344efad9f72c639c007ad25cc8f40617
kema-ray <rachelkemuma99@gmail.com>
2023-05-24 06:40:10 +0300

    script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    consoleCharacters(characters, 0);
+  }
+});
+
+function consoleCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        consoleCharacters(characters, index + 1);
+      }
+    }
+  });
+}
