ebb47477c7a98db2785c7aaba0dbe21c734d1143
kema-ray <rachelkemuma99@gmail.com>
2023-05-30 11:46:51 +0300

    fix code

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
 request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    consoleCharacters(characters, 0);
+    printCharacters(characters, 0);
   }
 });
 
-function consoleCharacters (characters, index) {
+function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
-        consoleCharacters(characters, index + 1);
+        printCharacters(characters, index + 1);
       }
     }
   });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const api = 'https://swapi-api.alx-tools.com/';
+const api = 'https://swapi-api.alx-tools.com/api/films/';
 const id = process.argv[2];
 
 request.get(api + id, function (error, result, body) {
   if (error) {
     console.log(error);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
-    console.log(JSON.parse(body).title);
+    console.log('Error code: ' + response.statusCode);
   }
 });
